8318ea193195e5635f437d39e67aef8e3e8a1a60
matt <twistybacon@outlook.co.nz>
2025-05-01 22:52:11 +1200

    added some stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Better terminal escape
+vim.keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    direction = "vertical", -- Set default to vertical split
+    size = 100,              -- Width of the vertical split (optional)
+    open_mapping = [[<leader>tt]], -- Optional: your toggle shortcut
+    dir = "cwd",
+  },
+}
