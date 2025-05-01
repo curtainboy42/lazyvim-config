08d17c73692882f0f27344552a48965ec73d2b91
mpot192 <mpot192@aucklanduni.ac.nz>
2025-05-01 22:56:11 +1200

    yes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   opts = {
     direction = "vertical", -- Set default to vertical split
-    size = 100,              -- Width of the vertical split (optional)
+    size = 90, -- Width of the vertical split (optional)
     open_mapping = [[<leader>tt]], -- Optional: your toggle shortcut
     dir = "cwd",
   },
