9f528195297afb1c6ddb88a948f211ab3d8c29f4
mpot192 <mpot192@aucklanduni.ac.nz>
2025-05-01 20:08:44 +1200

    yes

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("n", "<leader>vi", ":e $MYVIMRC<CR>", { desc = "Edit init.lua" })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+
+vim.keymap.set("n", "<leader>OO", "mzO<Esc>`z", { desc = "add blank line above" })
+vim.keymap.set("n", "<leader>oo", "mzo<Esc>`z", { desc = "add blank line below" })
